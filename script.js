3b10ada873537cf5a9cea211351028b78d6c3730
derushio <narumi18wa@gmail.com>
2019-01-03 17:24:52 +0900

    fix 編集モード解除タイミング

--- script.js
+++ script.js
@@ -66,8 +66,9 @@ map.on('pm:create', function(e) {
 map.on('pm:remove', function(e) {
     for (const i in polygons) {
         const polygon = polygons[i];
-        // 編集モード解除
-        // polygon.transform.disable();
+        if (e.layer === polygon) {
+            // 編集モード解除
+            polygon.transform.disable();
 
         if (e.layer === polygon) {
             //  削除されたポリゴンをリストから削除
